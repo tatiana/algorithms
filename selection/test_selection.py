df2887a284da85bb2a03ab422ed756308f222391
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2012-01-18 01:35:00 -0200

    ENH: Improved tests' names

--- selection/test_selection.py
+++ selection/test_selection.py
@@ -4,23 +4,25 @@ from selection import remove_smallest
 
 class SelectionByExtremeTestCase(unittest.TestCase):
 
-    def test_remove_smallest_1_range_1(self):
+    def test_remove_smallest_0_range_1(self):
         original_list = [0]
         expected = (0, [])
         answer = remove_smallest(original_list)
         self.assertEquals(expected, answer)
 
-    def test_remove_smallest_1_range_3(self):
+    def test_remove_smallest_0_range_3(self):
         original_list = [0, 1, 2]
         expected = (0, [1, 2])
         answer = remove_smallest(original_list)
         self.assertEquals(expected, answer)
 
-    def test_remove_smallest_1_raffled_range_5(self):
-        original_list = [4, 1, 3, 2, 0]
-        expected = (0, [4, 1, 3, 2])
+    def test_remove_smallest_1_raffled_range_1_to_6(self):
+        original_list = [4, 1, 3, 2, 5]
+        expected = (1, [4, 3, 2, 5])
         answer = remove_smallest(original_list)
         self.assertEquals(expected, answer)
+
+
 
 if __name__ == '__main__':
     unittest.main()
