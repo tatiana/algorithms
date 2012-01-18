ae3f073e689b47dc41101e2a8a3aa9be6f3a4d3c
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2012-01-18 00:14:57 -0200

    ADD: New tests for function that removes the smallest value from a list

--- selection/test_selection.py
+++ selection/test_selection.py
@@ -5,8 +5,20 @@ from selection import remove_smallest
 class SelectionByExtremeTestCase(unittest.TestCase):
 
     def test_remove_smaller_1_range_1(self):
-        original_list = range(1)  # [0]
+        original_list = [0]
         expected = []
+        answer = remove_smallest(original_list)
+        self.assertEquals(expected, answer)
+
+    def test_remove_smaller_1_range_3(self):
+        original_list = [0, 1, 2]
+        expected = [1, 2]
+        answer = remove_smallest(original_list)
+        self.assertEquals(expected, answer)
+
+    def test_remove_smaller_1_raffled_range_5(self):
+        original_list = [4, 1, 3, 2, 0]
+        expected = [4, 1, 3, 2]
         answer = remove_smallest(original_list)
         self.assertEquals(expected, answer)
 
