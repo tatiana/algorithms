2d6aa017896741bb19630cb10cc0a04c2da5a1d2
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2012-01-18 19:09:45 -0200

    DOC: Added doc strings to all existing tests

--- tests/test_selection.py
+++ tests/test_selection.py
@@ -5,6 +5,7 @@ from algorithms.selection import kth_by_extreme, kth_by_sorting
 class SelectionByExtremeTestCase(unittest.TestCase):
 
     def test_kth_by_extreme_list_2_return_2(self):
+        "Selection by Extreme: list=[2] kth=1 returns 2"
         original_list = [2]
         kth = 1
         expected = 2
@@ -12,6 +13,7 @@ class SelectionByExtremeTestCase(unittest.TestCase):
         self.assertEquals(expected, received)
 
     def test_kth_by_extreme_shuffled_range_1_10_return_3th_item(self):
+        "Selection by Extreme: list=[3, 5, 8, 4, 2, 7, 6, 1, 9] kth=3 returns 3"
         original_list = [3, 5, 8, 4, 2, 7, 6, 1, 9]
         expected = 3
         received = kth_by_extreme(original_list, 3)
@@ -21,6 +23,7 @@ class SelectionByExtremeTestCase(unittest.TestCase):
 class SelectionBySortingTestCase(unittest.TestCase):
 
     def test_kth_by_extreme_range_3_return_2th_item(self):
+        "Selection by Sorting: list=[0, 1, 2] kth=2 returns 1"
         original_list = [0, 1, 2]
         kth = 2
         expected = 1
@@ -28,6 +31,7 @@ class SelectionBySortingTestCase(unittest.TestCase):
         self.assertEquals(expected, received)
 
     def test_kth_by_extreme_unsorted_list_of_6_and_return_4th_item(self):
+        "Selection by Sorting: list=[5, 7, 10, 2, 6, 33] kth=4 returns 7"
         original_list = [5, 7, 10, 2, 6, 33]
         kth = 4
         expected = 7
@@ -35,9 +39,11 @@ class SelectionBySortingTestCase(unittest.TestCase):
         self.assertEquals(expected, received)
 
     def test_kth_by_extreme_shuffled_range_1_10_return_3th_item(self):
+        "Selection by Sorting: list=[3, 5, 8, 4, 2, 7, 6, 1, 9] kth=3 returns 3"
         original_list = [3, 5, 8, 4, 2, 7, 6, 1, 9]
+        kth = 3
         expected = 3
-        received = kth_by_sorting(original_list, 3)
+        received = kth_by_sorting(original_list, kth)
         self.assertEquals(expected, received)
 
 
--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -6,18 +6,21 @@ from algorithms.sorting import mergesort
 class MergeSortTestCase(unittest.TestCase):
 
     def test_sort_single_item(self):
+        "Sort by MergeSort: list=[4] returns [4]"
         shuffled = [4]
         expected = [4]
         received = mergesort(shuffled)
         self.assertEquals(expected, received)
 
     def test_sort_range_4(self):
+        "Sort by MergeSort: list=[3, 2, 1, 0] returns [0, 1, 2, 3]"
         shuffled = [3, 2, 1, 0]
         expected = [0, 1, 2, 3]
         received = mergesort(shuffled)
         self.assertEquals(expected, received)
 
     def test_sort_range_50(self):
+        "Sort by MergeSort: shuffled 50 items list"
         expected = range(50)
         shuffled = range(50)
         random.shuffle(shuffled)
