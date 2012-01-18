a75b0021bc827554a5bcd1fad71eb196aaf03ff1
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2012-01-17 23:58:34 -0200

    ADD: Test for function that removes the smallest value from a list

--- selection/test_selection.py
+++ selection/test_selection.py
@@ -1,10 +1,14 @@
 import unittest
+from selection import remove_smallest
 
 
-class SelectionTestCase(unittest.TestCase):
+class SelectionByExtremeTestCase(unittest.TestCase):
 
-    def test(self):
-        assert False
+    def test_remove_smaller_1_range_1(self):
+        original_list = range(1)  # [0]
+        expected = []
+        answer = remove_smallest(original_list)
+        self.assertEquals(expected, answer)
 
 if __name__ == '__main__':
     unittest.main()
