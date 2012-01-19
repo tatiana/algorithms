d3105c9800f1ee908c8e7be8c5853680971b3868
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2012-01-19 07:41:08 -0200

    ENH: Tests refactored (all implementations will be tested by a base suite)

--- tests/test_selection.py
+++ tests/test_selection.py
@@ -1,56 +1,58 @@
 import unittest
 from algorithms.selection import kth_by_extreme, kth_by_sorting, kth_by_mom
 
+# TODO: Try using unittest2 on this CommonTests class
+# http://docs.python.org/library/unittest.html#load-tests-protocol
+class CommonTests(object):
 
-class SelectionByExtremeTestCase(unittest.TestCase):
-
-    def test_kth_by_extreme_list_2_return_2(self):
-        "Selection by Extreme: list=[2] kth=1 returns 2"
+    def test_kth_list_2_return_2(self):
         original_list = [2]
         kth = 1
         expected = 2
-        received = kth_by_extreme(original_list, kth)
+        received = self.selection_func(original_list, kth)
         self.assertEquals(expected, received)
 
-    def test_kth_by_extreme_shuffled_range_1_10_return_3th_item(self):
-        "Selection by Extreme: list=[3, 5, 8, 4, 2, 7, 6, 1, 9] kth=3 returns 3"
-        original_list = [3, 5, 8, 4, 2, 7, 6, 1, 9]
-        expected = 3
-        received = kth_by_extreme(original_list, 3)
-        self.assertEquals(expected, received)
-
-
-class SelectionBySortingTestCase(unittest.TestCase):
-
-    def test_kth_by_sorting_range_3_return_2th_item(self):
+    def test_kth_range_3_return_2th_item(self):
         "Selection by Sorting: list=[0, 1, 2] kth=2 returns 1"
         original_list = [0, 1, 2]
         kth = 2
         expected = 1
-        received = kth_by_sorting(original_list, kth)
+        received = self.selection_func(original_list, kth)
         self.assertEquals(expected, received)
 
-    def test_kth_by_sorting_unsorted_list_of_6_and_return_4th_item(self):
-        "Selection by Sorting: list=[5, 7, 10, 2, 6, 33] kth=4 returns 7"
+    def test_kth_shuffled_list_of_6_and_return_4th_item(self):
         original_list = [5, 7, 10, 2, 6, 33]
         kth = 4
         expected = 7
-        received = kth_by_sorting(original_list, kth)
+        received = self.selection_func(original_list, kth)
         self.assertEquals(expected, received)
 
-    def test_kth_by_sorting_shuffled_range_1_10_return_3th_item(self):
-        "Selection by Sorting: list=[3, 5, 8, 4, 2, 7, 6, 1, 9] kth=3 returns 3"
+    def test_kth_shuffled_range_1_10_return_3th_item(self):
         original_list = [3, 5, 8, 4, 2, 7, 6, 1, 9]
         kth = 3
         expected = 3
-        received = kth_by_sorting(original_list, kth)
+        received = self.selection_func(original_list, kth)
         self.assertEquals(expected, received)
 
 
-class SelectionByMOMTestCase(unittest.TestCase):
+class SelectionByExtremeTestCase(unittest.TestCase, CommonTests):
+
+    def setUp(self):
+        self.selection_func = kth_by_extreme
+
+
+class SelectionBySortingTestCase(unittest.TestCase, CommonTests):
+
+    def setUp(self):
+        self.selection_func = kth_by_sorting
+
+
+class SelectionByMOMTestCase(unittest.TestCase, CommonTests):
+
+    def setUp(self):
+        self.selection_func = kth_by_mom
 
     def test_kth_by_mom_range_3_return_2th_item(self):
-        "Selection by MOM: list= range(1,16) kth=8 returns 8"
         original_list = range(1, 16)
         kth = 8
         expected = 8
