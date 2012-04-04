0c1daaf899a82532d34cb8d47c228afd273398b8
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2012-04-04 04:40:07 -0300

    ENH: Docstrings to data_structures module

--- algorithms/data_structures.py
+++ algorithms/data_structures.py
@@ -14,28 +14,60 @@ class Tree(object):
 
 
 class BinarySearchTree(Tree):
+    """
+    Binary search tree (BST), also known as ordered or sorted binary tree. It
+    is a node-based binary tree data structure which has the following
+    properties:
+        - The left subtree of a node contains only nodes with keys less than
+        the node's key.
+        - The right subtree of a node contains only nodes with keys greater
+        than the node's key.
+        - Both the left and right subtrees must also be binary search trees.
+
+    Example of a BST containing three nodes:
+        bst = BinarySearchTree()
+        bst.insert(2)
+        bst.insert(1)
+        bst.insert(3)
+
+    Builds the following tree:
+          2      # root
+        1   3    # leafs
+    """
 
     def __init__(self):
         self.root = None
         self.size = 0
 
     @property
-    def depth(self):
+    def height(self):
+        """
+        Return the number of links from the root node to the deepest node.
+        """
         return self.depth_from_node(self.root)
 
     def depth_from_node(self, node):
+        """
+        Return the number of links from the given @node to the deepest node.
+        """
         if node is None:
             return 0
         else:
-            ldepth = self.depth_from_node(node.left)
-            rdepth = self.depth_from_node(node.right)
-            return max(ldepth, rdepth) + 1
+            lheight = self.depth_from_node(node.left)
+            rheight = self.depth_from_node(node.right)
+            return max(lheight, rheight) + 1
 
     def insert(self, value):
+        """
+        Create a node with @value and add it from the root of the tree.
+        """
         self.root = self.insert_node(self.root, value)
         self.size += 1
 
     def insert_node(self, root, value):
+        """
+        Create a node with @value and add it to the subtree of a given @root.
+        """
         if root is None:
             return Node(value)
         elif root.value > value:
@@ -46,9 +78,15 @@ class BinarySearchTree(Tree):
 
     @property
     def max(self):
+        """
+        Retrieve node with maximum value.
+        """
         return self.max_from_node(self.root)
 
     def max_from_node(self, node):
+        """
+        Retrieve node with maximum value from the subtree where @node is root.
+        """
         if node is None:
             return False
         while node.right is not None:
@@ -57,9 +95,15 @@ class BinarySearchTree(Tree):
 
     @property
     def min(self):
+        """
+        Retrieve node with minimum value.
+        """
         return self.min_from_node(self.root)
 
     def min_from_node(self, node):
+        """
+        Retrieve node with minimum value from the subtree where @node is root.
+        """
         if node is None:
             return False
         while node.left is not None:
@@ -67,18 +111,30 @@ class BinarySearchTree(Tree):
         return node
 
     def predecessor(self, node):
+        """
+        Find the node which preceeds the given @node, based on the ordering
+        (this corresponds to return the rightmost node from the left subtree).
+        """
         return self.max_from_node(node.left)
 
     def to_string(self, node):
+        """
+        Return a string that represents graphically the sub-tree, considering
+        @node as root.
+
+           4
+         2   6
+        1 3 5 7
+        """
         output_string = ""
-        depth = self.depth_from_node(node)
+        height = self.depth_from_node(node)
         nodes_in_level = [node]
 
-        for level in xrange(depth):
+        for level in xrange(height):
             next_level_items = []
-            n_spaces = 2**(depth-level-1) - 1
+            n_spaces = 2**(height-level-1) - 1
             current_line = n_spaces * " "
-            n_spaces =  2**(depth-level) - 1
+            n_spaces =  2**(height-level) - 1
             middle_spaces = n_spaces * " "
 
             for item in nodes_in_level:
@@ -96,9 +152,21 @@ class BinarySearchTree(Tree):
         return output_string
 
     def query(self, value):
+        """
+        Retrieve a node with the @value from the root of the tree. If not
+        found, return False.
+
+        Note: Implemented using depth first search (DFS).
+        """
         return self.query_from_node(value, self.root)
 
     def query_from_node(self, value, node):
+        """
+        Retrieve a node with the @value from the @node subtree. If not found,
+        return False.
+
+        Note: Implemented using depth first search (DFS).
+        """
         if node is None:
             return False
         elif node.value == value:
@@ -109,17 +177,22 @@ class BinarySearchTree(Tree):
             return self.query_from_node(value, node.right)
 
     def remove(self, value):
+        """
+        Remove node with @value, searching from the tree root. If not found,
+        return False.
+        """
         node = self.query(value)
         if node == False:
             return False
         else:
-            self.root = self.remove_node(value)
+            self.root = self.remove_node(value, self.root)
             return self.root
 
-    def remove_node(self, value, node='root'):
-        if node == 'root':
-            node = self.root
-
+    def remove_node(self, value, node):
+        """
+        Remove node with @value, searching from  @node subtree. If not found,
+        return False.
+        """
         if node.value == value:
             if (node.left is None) and (node.right is None):
                 self.size -= 1
